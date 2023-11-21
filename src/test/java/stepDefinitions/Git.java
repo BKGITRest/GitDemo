458f05cb159aef2243136caddf9441621abbddff
Bharath <bharathk7@gmail.com>
2023-11-21 16:54:34 +0530

    First Change

---
+++ src/test/java/stepDefinitions/Git.java
@@ -0,0 +1,12 @@
+package stepDefinitions;
+
+public class Git {
+
+    public void GitDemo1() {
+        System.out.println("The Git Is Working");
+    }
+
+    public void GitDemo12() {
+        System.out.println("The Git Is Not Working");
+    }
+}
