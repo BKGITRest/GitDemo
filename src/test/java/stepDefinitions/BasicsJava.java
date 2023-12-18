e7c75e1f8003420ba6f17288f7d0384467f12a55
Bharath <bharathk7@gmail.com>
2023-12-18 16:23:14 +0530

    Java Changes

--- src/test/java/stepDefinitions/BasicsJava.java
+++ src/test/java/stepDefinitions/BasicsJava.java
@@ -1,5 +1,7 @@
 package stepDefinitions;
 
+import groovy.xml.StreamingDOMBuilder;
+
 import static stepDefinitions.ReusableMethods.validateHeader;
 
 public class BasicsJava {
@@ -13,9 +15,30 @@ public class BasicsJava {
         int b = 5;
         int sum = a + b;
 
-        System.out.println("The Sum Of the Values :"+ sum);
+        System.out.println("The Sum Of the Values :" + sum);
 
         //Return Type
         System.out.println(validateHeader());
+
+        //String Types
+        String str = "Payment Is Done";
+        System.out.println("The char at repective index is :" + str.charAt(8));
+        System.out.println("The index of specific char :" + str.indexOf("D"));
+        System.out.println("The substring is :" + str.substring(8));
+
+        //Print the String in Reverse
+        String s = "malayalam";
+        String t = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            t = t + s.charAt(i);
+        }
+        System.out.println(t);
+
+        if (s == t) {
+            System.out.println("The Given String Is palindrome");
+        }
+
     }
 }
+
+
